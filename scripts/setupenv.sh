5b8f2e9e109b6112ff9ce8cf7d024d721e12eb47
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-06-14 14:14:45 -0400

    Some fixes in setupenv script

--- scripts/setupenv.sh
+++ scripts/setupenv.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-cd $HOME/inkbox/kernel/toolchain/gcc-4.8/bin/
-export PATH=$PATH:$PWD
-cd -
+if [ -z "$GITDIR" ]; then
+	echo "Please specify the GITDIR environment variable."
+	return 1
+else
+	cd $GITDIR/toolchain/gcc-4.8/bin/
+	export PATH=$PATH:$PWD
+	cd -
+	cd $GITDIR/toolchain/armv7l-linux-musleabihf-cross/bin
+	export PATH=$PATH:$PWD
+	cd -
 
-echo "Environment set up."
+	echo "Environment set up."
+fi
