388c23de7d65b14761c59e78a4c6cf80442b26d2
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-07-08 22:14:01 -0400

    Add default InkBox kernel config for Libra N873

--- scripts/setupenv.sh
+++ scripts/setupenv.sh
@@ -4,11 +4,14 @@ if [ -z "$GITDIR" ]; then
 	echo "Please specify the GITDIR environment variable."
 	return 1
 else
-	cd $GITDIR/toolchain/gcc-4.8/bin/
-	export PATH=$PATH:$PWD
+	cd "${GITDIR}/toolchain/gcc-4.8/bin/"
+	export PATH="${PATH}:${PWD}"
 	cd -
-	cd $GITDIR/toolchain/armv7l-linux-musleabihf-cross/bin
-	export PATH=$PATH:$PWD
+	cd "${GITDIR}/toolchain/armv7l-linux-musleabihf-cross/bin"
+	export PATH="${PATH}:${PWD}"
+	cd -
+	cd "${GITDIR}/toolchain/arm-nickel-linux-gnueabihf/bin"
+	export PATH="${PATH}:${PWD}"
 	cd -
 
 	echo "Environment set up."
