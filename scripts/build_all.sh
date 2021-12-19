65f9cc2c7b592bbd16f9705e4cf47d10c0a0547d
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-12-18 23:11:16 -0500

    Tweaks in build_all.sh script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -21,6 +21,9 @@ $GITDIR/scripts/make_devicenodes.sh
 $GITDIR/scripts/build_kernel.sh n705 std
 $GITDIR/scripts/build_kernel.sh n705 root
 $GITDIR/scripts/build_kernel.sh n705 diags
+$GITDIR/scripts/build_kernel.sh n905b std
+$GITDIR/scripts/build_kernel.sh n905b root
+$GITDIR/scripts/build_kernel.sh n905b diags
 $GITDIR/scripts/build_kernel.sh n905c std
 $GITDIR/scripts/build_kernel.sh n905c root
 $GITDIR/scripts/build_kernel.sh n905c diags
