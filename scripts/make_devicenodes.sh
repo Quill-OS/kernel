88080722fb81694441feed551b1e9156d5feccf9
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-01-03 13:48:33 -0500

    sunxi: add bpi to make_devicenodes.sh script

--- scripts/make_devicenodes.sh
+++ scripts/make_devicenodes.sh
@@ -7,10 +7,13 @@ else
 	make_nodes() {
 		mkdir -p dev
 		sudo mknod dev/console c 5 1
-		sudo mknod dev/ttymxc0 c 207 16
 		sudo mknod dev/null c 1 3
 		if [ "${1}" == "emu" ]; then
 			sudo mknod dev/ttyAMA0 c 204 64
+		elif [ "${1}" == "bpi" ]; then
+			sudo mknod dev/ttyS0 c 4 64
+		else
+			sudo mknod dev/ttymxc0 c 207 16
 		fi
 	}
 
@@ -40,5 +43,9 @@ else
 	make_nodes emu 2>/dev/null
 	cd ${GITDIR}/initrd/emu-diags
 	make_nodes emu 2>/dev/null
+	cd ${GITDIR}/initrd/bpi
+	make_nodes bpi 2>/dev/null
+	cd ${GITDIR}/initrd/bpi-diags
+	make_nodes bpi 2>/dev/null
 	echo "Done making devices nodes."
 fi
