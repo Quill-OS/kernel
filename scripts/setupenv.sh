622ccddb23f9df55dd94474962fdd8fd6158b273
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-05-01 22:02:47 -0400

    N249: Install modules and public key

--- scripts/setupenv.sh
+++ scripts/setupenv.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-if [ -z "$GITDIR" ]; then
+if [ -z "${GITDIR}" ]; then
 	echo "Please specify the GITDIR environment variable."
 	return 1
 else
-	cd "${GITDIR}/toolchain/gcc-4.8/bin/"
+	pushd "${GITDIR}/toolchain/gcc-4.8/bin/"
 	export PATH="${PATH}:${PWD}"
-	cd -
-	cd "${GITDIR}/toolchain/armv7l-linux-musleabihf-cross/bin"
+	popd
+	pushd "${GITDIR}/toolchain/armv7l-linux-musleabihf-cross/bin"
 	export PATH="${PATH}:${PWD}"
-	cd -
-	cd "${GITDIR}/toolchain/arm-nickel-linux-gnueabihf/bin"
+	popd
+	pushd "${GITDIR}/toolchain/arm-nickel-linux-gnueabihf/bin"
 	export PATH="${PATH}:${PWD}"
-	cd -
+	popd
+	pushd "${GITDIR}/toolchain/arm-kobo-linux-gnueabihf/bin"
+	export PATH="${PATH}:${PWD}"
+	popd
 
 	echo "Environment set up."
 fi
